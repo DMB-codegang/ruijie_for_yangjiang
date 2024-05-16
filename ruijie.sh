2b686c7797e0f74b5dc52ef9db9bcebad6713932
小舍 <84826021+DMB-codegang@users.noreply.github.com>
2024-05-16 12:54:21 +0800

    Update ruijie.sh
    为认证添加容错功能

--- ruijie.sh
+++ ruijie.sh
@@ -11,7 +11,6 @@ fi
 #检查是否可以联网，可以联网就退出登录
 captiveReturnCode=`curl -s -I -m 10 -o /dev/null -s -w %{http_code} http://www.google.cn/generate_204`
 if [ "${captiveReturnCode}" = "204" ]; then
-  echo "You are already online!"
   echo "您已经正常联网(●'◡'●)"
   exit 0
 fi
@@ -21,8 +20,10 @@ fi
 #获得Ruijie的loginURL
 loginPageURL=`curl -s "http://www.google.cn/generate_204" | awk -F \' '{print $2}'`
 if [ -z "${loginPageURL}" ]; then
-  #有的时候会获取不到，直接拉过来得了（写法存疑）
-  loginPageURL="http://10.20.252.12/eportal/index.jsp?wlanuserip=8a642879232d159f7913d3d3aef47d33&wlanacname=16b0c0f4b777150ca8bdcf1c6c309281&ssid=e8665acbe24d304994c8729c8726080b&nasip=3ee885a481183d32df59bb65fa765ed5&mac=0a5ce2a1551e7103bafeb3b7358229f7&t=wireless-v2&url=7b83b64d9f59ab6db231ba35b252f2c1a180d7445f9aa38b79119140b86fb3f48548a1b2660ae802"
+  #有的时候会获取不到，直接拉过来得了（写法存疑，如果有需要可以取消这里的注释）
+  #loginPageURL="http://10.20.252.12/eportal/index.jsp?wlanuserip=8a642879232d159f7913d3d3aef47d33&wlanacname=16b0c0f4b777150ca8bdcf1c6c309281&ssid=e8665acbe24d304994c8729c8726080b&nasip=3ee885a481183d32df59bb65fa765ed5&mac=0a5ce2a1551e7103bafeb3b7358229f7&t=wireless-v2&url=7b83b64d9f59ab6db231ba35b252f2c1a180d7445f9aa38b79119140b86fb3f48548a1b2660ae802"
+  echo "无法获取校园网登录链接，请检查与校园网的连接是否正常"
+  exit 0
 fi
 
 #具有阳职特色的service
@@ -41,4 +42,7 @@ queryString="${queryString//=/%253D}"
 if [ -n "${loginURL}" ]; then
   authResult=`curl -s -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.91 Safari/537.36" -e "${loginPageURL}" -b "EPORTAL_COOKIE_USERNAME=; EPORTAL_COOKIE_PASSWORD=; EPORTAL_COOKIE_SERVER=; EPORTAL_COOKIE_SERVER_NAME=; EPORTAL_AUTO_LAND=; EPORTAL_USER_GROUP=; EPORTAL_COOKIE_OPERATORPWD=;" -d "userId=${1}&password=${2}&service=${service}&queryString=${queryString}&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" -H "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" "${loginURL}"`
   echo $authResult
+    if [ `grep -c "认证失败" $authResult` -ne '0' ]; then
+    reboot
+  fi
 fi
